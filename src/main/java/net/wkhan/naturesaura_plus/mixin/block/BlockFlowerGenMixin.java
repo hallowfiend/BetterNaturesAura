e24a85dfc128a3afcf0fa2d2e41dd3aa1bb6df22
Waqar Ahmad Khan <wak4746@gmail.com>
2026-06-19 19:09:41 +0530

    flower and moss gen, fixed bounding boxes display

---
+++ src/main/java/net/wkhan/naturesaura_plus/mixin/block/BlockFlowerGenMixin.java
@@ -0,0 +1,32 @@
+package net.wkhan.naturesaura_plus.mixin.block;
+
+import de.ellpeck.naturesaura.blocks.BlockContainerImpl;
+import de.ellpeck.naturesaura.blocks.BlockFlowerGenerator;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.phys.AABB;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import static net.wkhan.naturesaura_plus.Config.flowerGenRange;
+
+@Mixin(BlockFlowerGenerator.class)
+public abstract class BlockFlowerGenMixin extends BlockContainerImpl {
+    public BlockFlowerGenMixin(String baseName, Class<? extends BlockEntity> tileClass, Properties properties) {
+        super(baseName, tileClass, properties);
+    }
+
+    @Inject(
+            method = "getVisualizationBounds",
+            at = @At("HEAD"),
+            cancellable = true,
+            remap = false
+    )
+    private void naturesaura_plus$visualizationBoundsFlowerGen(Level level, BlockPos pos, CallbackInfoReturnable<AABB> cir) {
+        cir.cancel();
+        cir.setReturnValue((new AABB(pos)).inflate(flowerGenRange,1,flowerGenRange));
+    }
+}
---
+++ src/main/java/net/wkhan/naturesaura_plus/mixin/block/BlockMossGenMixin.java
@@ -0,0 +1,33 @@
+package net.wkhan.naturesaura_plus.mixin.block;
+
+import de.ellpeck.naturesaura.blocks.BlockContainerImpl;
+import de.ellpeck.naturesaura.blocks.BlockMossGenerator;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.phys.AABB;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import static net.wkhan.naturesaura_plus.Config.mossGenRange;
+
+@Mixin(BlockMossGenerator.class)
+public abstract class BlockMossGenMixin extends BlockContainerImpl {
+    public BlockMossGenMixin(String baseName, Class<? extends BlockEntity> tileClass, Properties properties) {
+        super(baseName, tileClass, properties);
+    }
+
+    @Inject(
+            method = "getVisualizationBounds",
+            at = @At("HEAD"),
+            cancellable = true,
+            remap = false
+    )
+    private void naturesaura_plus$visualizationBoundsMossGen(Level level, BlockPos pos, CallbackInfoReturnable<AABB> cir) {
+        cir.cancel();
+        cir.setReturnValue((new AABB(pos)).inflate(mossGenRange));
+    }
+
+}
