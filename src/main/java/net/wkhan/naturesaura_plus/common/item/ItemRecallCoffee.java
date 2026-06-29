9269dfef4e988c24e895869107a55742e2d29093
Waqar Ahmad Khan <wak4746@gmail.com>
2026-06-29 15:10:05 +0530

    teleport with pets to spawn with recall coffee

--- src/main/java/net/wkhan/naturesaura_plus/common/item/ItemRecallCoffee.java
+++ src/main/java/net/wkhan/naturesaura_plus/common/item/ItemRecallCoffee.java
@@ -1,5 +1,7 @@
 package net.wkhan.naturesaura_plus.common.item;
 
+import de.ellpeck.naturesaura.packet.PacketHandler;
+import de.ellpeck.naturesaura.packet.PacketParticles;
 import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
@@ -7,6 +9,9 @@ import net.minecraft.network.chat.Style;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.TamableAnimal;
+import net.minecraft.world.entity.ai.targeting.TargetingConditions;
+import net.minecraft.world.entity.animal.horse.AbstractHorse;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -27,15 +32,39 @@ public class ItemRecallCoffee extends Item {
     public record ServerLevelVec3SpawnAnglePack(ServerLevel serverLevel, Vec3 pos, float angle) {}
 
     @Override
-    public @NotNull ItemStack finishUsingItem(@NotNull ItemStack stack, Level level, @NotNull LivingEntity entity) {
+    public @NotNull ItemStack finishUsingItem(@NotNull ItemStack stack, Level level, @NotNull LivingEntity entity) { //particles no work, no bueno
         if(level.isClientSide) return super.finishUsingItem(stack, level, entity);
         if(!(entity instanceof Player player)) return super.finishUsingItem(stack, level, entity);
         ServerLevelVec3SpawnAnglePack levelNVec3 = simulateRespawnCheck(player);
         Vec3 spawnPos = levelNVec3.pos;
-        ServerLevel serverLevel = levelNVec3.serverLevel; //(serverLevel, spawnPos.x, spawnPos.y, spawnPos.z)
+        ServerLevel serverLevel = levelNVec3.serverLevel;
         float spawnAngle = levelNVec3.angle;
         ServerPlayer serverPlayer = (ServerPlayer) player;
+        int petTpRange = 2; //make config
+        List<LivingEntity> pets = serverLevel.getNearbyEntities(LivingEntity.class,
+                TargetingConditions.forNonCombat().ignoreInvisibilityTesting().ignoreLineOfSight().range(petTpRange).selector(
+                        target -> {
+                            if (target instanceof TamableAnimal pet)
+                                return serverPlayer.getUUID() == pet.getOwnerUUID();
+                            else if (target instanceof AbstractHorse horse)
+                                return serverPlayer.getUUID() == horse.getOwnerUUID();
+                            return false;
+                        }
+                ), serverPlayer, serverPlayer.getBoundingBox().inflate(petTpRange));
+
+        pets.forEach(pet -> {
+            PacketHandler.sendToAllAround(serverLevel, BlockPos.containing(pet.position()), 32,
+                    new PacketParticles((float)pet.getX(), (float)pet.getY(), (float)pet.getZ(), PacketParticles.Type.PET_REVIVER));
+            pet.teleportTo(spawnPos.x, spawnPos.y, spawnPos.z);
+        });
+
+        PacketHandler.sendToAllAround(serverLevel, BlockPos.containing(serverPlayer.position()), 32,
+                new PacketParticles((float)serverPlayer.getX(), (float)serverPlayer.getY(), (float)serverPlayer.getZ(), PacketParticles.Type.PET_REVIVER));
         serverPlayer.teleportTo(serverLevel, spawnPos.x, spawnPos.y, spawnPos.z, spawnAngle, 0.0F);
+
+        PacketHandler.sendToAllAround(serverLevel, BlockPos.containing(spawnPos), 32,
+                new PacketParticles((float) spawnPos.x,(float) spawnPos.y,(float) spawnPos.z, PacketParticles.Type.PET_REVIVER));
+
         return super.finishUsingItem(stack, level, entity);
     }
 
